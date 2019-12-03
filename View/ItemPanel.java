fce7b218e0d1960413e8ab1d3c7c4d9df9a1ee33
saintfortdan <34300254+saintfortdan@users.noreply.github.com>
2019-12-03 00:18:08 -0500

    Add files via upload

---
+++ View/ItemPanel.java
@@ -0,0 +1,26 @@
+package views;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import Model.*;
+
+public abstract class ItemPanel extends View
+{
+	JButton name;
+	JLabel Price;
+	JLabel Stock;
+	JButton submit;
+	JTextField Qty;
+	JLabel Description;
+	JPanel panel = new JPanel();
+	
+	
+	ItemPanel(Item item){}
+	
+	public abstract void createItemWindow();
+	
+	String readQty()
+	{return Qty.getText();}
+
+}
