35f6cf38a1aa724adc43bfcf3850f10c4390b890
saintfortdan <34300254+saintfortdan@users.noreply.github.com>
2019-12-03 00:29:14 -0500

    Update test.java

--- View/test.java
+++ View/test.java
@@ -3,8 +3,10 @@ package View;
 public class test {
     public static void main(String [] args)
     {
-    	View test = new View();
-    	test.Display();
+    	Login Welcome = new Login();
+    	Welcome.Frame();
+    	//View test = new View(2);
+    	//test.Display();
     }
 
 }
